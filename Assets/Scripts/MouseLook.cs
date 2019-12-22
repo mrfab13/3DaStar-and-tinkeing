38654ade3d2772bd5d2f6d3e98cbf6acb048f4b3
Vaughan Webb <vaughanw2012@gmail.com>
2019-12-22 17:52:00 +1300

    init

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSens = 100.0f;
+
+
+    void Update()
+    {
+
+    }
+}
