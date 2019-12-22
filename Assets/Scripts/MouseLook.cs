551fc13d353d589887ef385a3b78a13c1ee475fd
Vaughan Webb <vaughanw2012@gmail.com>
2019-12-22 21:54:23 +1300

    good mouse look

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -5,10 +5,29 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     public float mouseSens = 100.0f;
+    public float mouseX;
+    public float mouseY;
+    float pitch;
+    public Transform body;
 
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     void Update()
     {
+        float yaw = transform.localEulerAngles.y;
+ 
+        yaw += Input.GetAxis("Mouse X") * mouseSens;
 
+        float pitchDelta = -Input.GetAxis("Mouse Y") * mouseSens;
+        pitch += pitchDelta;
+        pitch = Mathf.Clamp(pitch, -89.9f, 89.9f);
+
+        transform.localEulerAngles = new Vector3(pitch, 0, 0);
+
+        body.Rotate(Vector3.up, yaw);
     }
 }
