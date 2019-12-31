4d718e0ef7e2f6906f5d57e904f70648776a1b8d
Vaughan Webb <vaughanw2012@gmail.com>
2019-12-31 18:14:00 +1300

    look at script not quite working

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class enemy : MonoBehaviour
+{
+    public float max = 100.0f;
+    public float hp = 100.0f;
+
+    void Update()
+    {
+        gameObject.transform.GetChild(0).GetChild(0).GetComponent<Image>().fillAmount = hp / max;
+
+        if (hp <= 0.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/lookat.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookat : MonoBehaviour
+{
+    public Transform target;
+    void Update()
+    {
+
+
+        transform.LookAt(new Vector3(target.position.x, gameObject.transform.position.y + gameObject.GetComponentInParent<Transform>().position.y, target.position.z));
+    }
+}
