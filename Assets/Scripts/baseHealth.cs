ef50bd7a9659276f621e641f0d1e1986b4fc620d
Vaughan Webb <vaughanw2012@gmail.com>
2020-01-04 18:02:26 +1300

    reading enem,y

---
+++ Assets/Scripts/baseHealth.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class baseHealth : MonoBehaviour
+{
+
+    float Health = 100.0f;
+
+
+}
--- Assets/Scripts/spawner.cs
+++ Assets/Scripts/spawner.cs
@@ -17,6 +17,11 @@ public class spawner : MonoBehaviour
     public float timer = 0.0f;
     public float trailSpawnerDelay = 1.0f;
 
+    public int currWave = 0;
+    public List<GameObject> waves;
+
+    bool onceWave = false;
+
     void Update()
     {
         if (currentGameSatae == gamestate.pre)
@@ -26,6 +31,28 @@ public class spawner : MonoBehaviour
             {
                 timer = 0.0f;
                 Instantiate(trail, this.transform.position, Quaternion.identity);
+            }
+
+            if (onceWave == true)
+            {
+                onceWave = false;
+            }
+        }
+
+        if (currentGameSatae == gamestate.wave)
+        {
+            if (onceWave == false)
+            {
+                timer = 0.0f;
+                onceWave = true;
+                currWave++;
+            }
+
+            timer += Time.deltaTime;
+            if (timer >= trailSpawnerDelay)
+            {
+                timer = 0.0f;
+                Instantiate(waves[currWave], this.transform.position, Quaternion.identity);
             }
         }
     }
