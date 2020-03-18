95e37d0f0b57d276cddd44cdbce89377b1582a45
Vaughan Webb <vaughanw2012@gmail.com>
2020-03-18 20:48:42 +1300

    dll test

---
+++ Assets/Scripts/dlltest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.IO;
+
+public class dlltest : MonoBehaviour
+{
+    [DllImport("TEST1", EntryPoint = "TestDivide")]
+    public static extern float StraightFromDllTestDivide(float a, float b);
+
+    void Start()
+    {
+        float straightFromDllDivideResult = StraightFromDllTestDivide(20, 5);
+
+        Debug.Log(straightFromDllDivideResult);
+
+    }
+
+}
