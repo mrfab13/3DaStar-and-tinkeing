555c8b8f7d69ea3f647651c40af2c231a74ef690
Vaughan Webb <vaughanw2012@gmail.com>
2020-04-23 12:54:42 +1200

    dll upgrade to static

---
+++ Assets/Scripts/plugins/iamryan.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class iamryan : MonoBehaviour
+{
+    void Start()
+    {
+        float ans = dlltest.mult(1, 2);
+        Debug.Log(ans);
+    }
+
+    void Update()
+    {
+        
+    }
+}
