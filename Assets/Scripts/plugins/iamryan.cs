ebb8b21727bd124aff2fcb7bd01425bd3dfe3f5c
Vaughan Webb <vaughanw2012@gmail.com>
2020-04-30 17:40:52 +1200

    work done

--- Assets/Scripts/plugins/iamryan.cs
+++ Assets/Scripts/plugins/iamryan.cs
@@ -1,14 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using pathfind;
 
 public class iamryan : MonoBehaviour
 {
-    void Start()
+    public List<GameObject> boxes = new List<GameObject>() { };
+    public GameObject nodePrefab;
+    public float deets;
+
+    public bool reclaculatepath;
+
+    private void Start()
     {
-        float ans = dlltest.mult(1, 2);
-        Debug.Log(ans);
+        Path.nodeprefab = nodePrefab;
+        for (int i = 0; i < boxes.Count; i++)
+        {
+            Path.boxes.Add(boxes[i]);
+        }
     }
 
 
+    private void Update()
+    {
+        if (reclaculatepath == true)
+        {
+            recalculate();
+        }
+    }
+
+    public void redrawnodes()
+    {
+
+
+        GameObject[] count = GameObject.FindGameObjectsWithTag("node");
+
+        for (int i = count.Length; i > 0; i--)
+        {
+            Destroy(count[i - 1]);
+        }
+
+        Path.drawNodes();
+    }
+
+    public void recalculate()
+    {
+        
+        Path.detail = deets;
+
+        Path.CalculatePath();
+        reclaculatepath = false;
+
+        redrawnodes();
+    }
 }
