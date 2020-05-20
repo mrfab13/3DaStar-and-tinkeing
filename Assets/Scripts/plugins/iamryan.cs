23a8ec237272b72cea2e3288595321ca4ab459f8
Vaughan Webb <vaughanw2012@gmail.com>
2020-05-20 17:42:00 +1200

    can find gameobjects
    need to update if path blocked or things moved

--- Assets/Scripts/plugins/iamryan.cs
+++ Assets/Scripts/plugins/iamryan.cs
@@ -2,21 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using pathfind;
+using UnityEditor;
 
 public class iamryan : MonoBehaviour
 {
-    public GameObject boxes;
-    public GameObject nodePrefab;
     public float deets;
     public GameObject source;
+    public GameObject destination;
+
     public bool reclaculatepath;
     public bool movment = false;
     public float movespeed;
 
+    private window editwindow;
     private void Start()
     {
-        Path.nodeprefab = nodePrefab;
-        Path.thebox = boxes;
         Path.source = source;
     }
 
@@ -35,34 +35,39 @@ public class iamryan : MonoBehaviour
         }
     }
 
-    public void redrawnodes()
-    {
-
-
-        GameObject[] count = GameObject.FindGameObjectsWithTag("node");
-
-        for (int i = count.Length; i > 0; i--)
-        {
-            Destroy(count[i - 1]);
-        }
-
-        Path.drawNodes();
-    }
-
     public void recalculate()
     {
-        
+        Path.startphysical = source.transform.position;
+        Path.finishphysical = destination.transform.position;
+
+        Path.BBbounds = editwindow.testbounds;
+        Path.BBbounds.extents = new Vector3((Path.BBbounds.extents.x / 2.0f), (Path.BBbounds.extents.y / 2.0f), (Path.BBbounds.extents.z / 2.0f));
         Path.detail = deets;
         reclaculatepath = false;
 
         Path.CalculatePath();
 
-        redrawnodes();
     }
 
 
     void OnDrawGizmos()
     {
+        if (editwindow == null)
+        {
+            editwindow = (window)EditorWindow.GetWindow(typeof(window));
+        }
+        Gizmos.DrawWireCube(editwindow.testbounds.center, editwindow.testbounds.extents);
+
+
+        for (int i = 0; i < Path.nodes.Count; i++)
+        {
+            if (Path.nodes[i].getwalkable())
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawCube(Path.nodes[i].returnpos(), new Vector3((Path.detail / 8.0f), (Path.detail / 8.0f), (Path.detail / 8.0f)));
+            }
+        }
+
         for (int i = 0; i < Path.pathlist.Count; i++)
         {
             Gizmos.color = Color.blue;
---
+++ Assets/Scripts/plugins/window.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Text.RegularExpressions;
+
+public class window : EditorWindow
+{
+    public string stign = "im a text box for fun :D";
+    public bool groupEnabled;
+    public bool testbool = true;
+    public float testfloat = 1.5f;
+    public Bounds testbounds;
+
+
+    [MenuItem("Window/PathFinding")]
+    static void init()
+    {
+        window tmp = (window)EditorWindow.GetWindow(typeof(window));
+        tmp.Show();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("base settings", EditorStyles.boldLabel);
+        stign = EditorGUILayout.TextField("text fild", stign);
+
+        testbounds = EditorGUILayout.BoundsField("bounds box", testbounds);
+
+        groupEnabled = EditorGUILayout.BeginToggleGroup("optinL settigns", groupEnabled);
+        testbool = EditorGUILayout.Toggle("toggler", testbool);
+        testfloat = EditorGUILayout.FloatField("zoop", testfloat);
+        EditorGUILayout.EndToggleGroup();
+    }
+
+}
